7689a88c8b04c91d4a3c48381d0061f4a91c8099
fria-li <102595969+fria-li@users.noreply.github.com>
2022-03-29 14:49:17 +0200

    Add files via upload

---
+++ EvenOdd.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"please, Enter a number: ";
+    cin>>num;
+    if (num%2!=0){
+        cout<<num<<" is odd";
+    }
+}
