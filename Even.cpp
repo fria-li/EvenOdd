34e992ac240417476f1329706b0f993742138d99
FarahHany0 <101879380+FarahHany0@users.noreply.github.com>
2022-03-29 15:09:26 +0200

    Add files via upload

---
+++ Even.cpp
@@ -0,0 +1,15 @@
+/*
+Author: Farah Hany
+Mail: xxxxxxxx
+Description: identifying even numbers.
+ */
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"please, Enter a number: ";
+    cin>>num;
+    if (num%2==0){
+        cout<<num<<" is even";
+    }
+}
